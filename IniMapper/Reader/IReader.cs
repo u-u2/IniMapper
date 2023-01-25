8beadefcc16f715859cd0293c0935fdceb5635b0
u-u2 <u-u2@outlook.jp>
2023-01-25 16:35:36 +0900

    doc add comment

--- IniMapper/Reader/IReader.cs
+++ IniMapper/Reader/IReader.cs
@@ -3,10 +3,25 @@
 namespace IniMapper.Reader {
 	public interface IReader {
 
+		/// <summary>
+		/// Read all sections.
+		/// </summary>
+		/// <returns></returns>
 		List<Section> ReadSections();
 
+
+		/// <summary>
+		/// Read as <see cref="Ini"/>.
+		/// </summary>
+		/// <returns></returns>
 		Ini ReadIni();
 
+
+		/// <summary>
+		/// Read as <see cref="T"/>
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
 		T ReadValue<T>() where T : new();
 
 	}
--- IniMapper/Writer/IWriter.cs
+++ IniMapper/Writer/IWriter.cs
@@ -4,20 +4,20 @@ namespace IniMapper.Writer {
 	internal interface IWriter {
 
 		/// <summary>
-		/// write section. insert brackets automatically.
+		/// Write section. insert brackets automatically.
 		/// </summary>
 		/// <param name="name"></param>
 		public void WriteSection(string name);
 
 		/// <summary>
-		/// write key value pair. insert equal sign automatically.
+		/// Write key value pair. insert equal sign automatically.
 		/// </summary>
 		/// <param name="key"></param>
 		/// <param name="value"></param>
 		public void WriteProperty(string key, string value);
 
 		/// <summary>
-		/// write ini data.
+		/// Write ini.
 		/// </summary>
 		/// <param name="ini"></param>
 		public void Write(Ini ini);
